28a55a4df8eb761d3c9efa0048a4930459317350
guizhenwg <wg15201369698@163.com>
2018-06-04 19:06:35 +0800

    00000

--- app/src/main/java/com/example/myapplication0/MainActivity.java
+++ app/src/main/java/com/example/myapplication0/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //477477+98887+998+98+798+89+897897
+        // 000000000
     }
 }
